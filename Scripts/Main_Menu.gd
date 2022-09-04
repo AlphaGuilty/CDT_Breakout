81614857c19b8761d9b29339a5a24336f7dc9281
despinoza9 <despinoza1999@hotmail.com>
2022-09-03 23:39:16 -0400

    Boton play
    Se modifico el script del boton play

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -6,11 +6,15 @@ onready var htp = $VBoxContainer/HtP
 onready var exit = $VBoxContainer/Exit
 
 func _ready():
+	start.connect('pressed', self, "_start_on_pressed")
 	htp.connect('pressed', self, "_htp_on_pressed")
 	credit.connect('pressed', self, "_credit_on_pressed")
 	exit.connect('pressed', self, "_exit_on_pressed")	
 	pass 
 
+func _start_on_pressed():
+	get_tree().change_scene("res://Scene/Demo.tscn")
+	
 func _htp_on_pressed():
 	get_tree().change_scene("res://Scene/HowtoPlay.tscn")
 
