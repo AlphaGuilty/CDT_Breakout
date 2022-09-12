a04d71b69432bc586fd793f150ef5129fdf1b5ad
despinoza9 <despinoza1999@hotmail.com>
2022-09-11 21:08:23 -0300

    pausa
    agregue el menú de pausa

---
+++ Scripts/pause.gd
@@ -0,0 +1,33 @@
+extends MarginContainer
+
+onready var continu = $VBoxContainer/Continue
+onready var menu = $VBoxContainer/Menu
+onready var how = $VBoxContainer/HtP
+onready var exit = $VBoxContainer/Exit
+
+func _ready():
+	continu.connect("pressed",self,"on_continu_pressed")
+	menu.connect("pressed",self,"on_menu_pressed")
+	how.connect("pressed",self,"on_how_pressed")
+	exit.connect("pressed",self,"on_exit_pressed")
+	hide()
+	
+func _input(event):
+	if event.is_action_pressed("menu"):
+		visible= !visible
+		get_tree().paused=visible
+	
+func on_continu_pressed():
+	hide()
+	get_tree().paused=false
+	
+func on_menu_pressed():
+	get_tree().paused=false
+	get_tree().change_scene("res://Scene/Main_Menu.tscn")
+	
+func on_how_pressed():
+	get_tree().paused=false
+	get_tree().change_scene("res://Scene/HowToPlay.tscn")
+	
+func on_exit_pressed():
+	get_tree().quit()
