8076ea91a6b60454e584e1894328142de0868c08
Oil Limk <camilo.urzua@ug.uchile.cl>
2022-09-23 16:08:34 -0300

    Bricks & Scripts
    Se creó la escena brick para los ladrillos con su textura y movimiento. Se añadió a las demos. Además se dejó los scripts de bar y brick en sus escenas respectivas.

---
+++ Scripts/brick.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+
+var SPEED = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func _physics_process(delta):
+	var move_input = Input.get_axis("P2_move_left","P2_move_right")
+	
+	velocity.x = move_input * SPEED 
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
