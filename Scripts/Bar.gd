9c2f760860ec697ce81b5b04d05bc8afc86c38da
Oil Limk <camilo.urzua@ug.uchile.cl>
2022-09-24 13:06:21 -0300

    limpiar
    se eliminaron comentarios no importantes en scripts

--- Scripts/Bar.gd
+++ Scripts/Bar.gd
@@ -4,11 +4,6 @@ var velocity = Vector2()
 
 var SPEED = 500
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,8 +14,3 @@ func _physics_process(delta):
 	velocity.x = move_input * SPEED 
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
