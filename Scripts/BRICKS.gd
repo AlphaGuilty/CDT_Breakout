deb141a696f5de9a2fc94ffd16a0647f6cd13083
Oil Limk <camilo.urzua@ug.uchile.cl>
2022-10-05 22:45:36 -0300

    Grupo de Ladrillos
    Se implementa grupo de ladrillos, se juntan las paredes y el techo de la demo en una sola escena y se elimina la demo_2.

--- Scripts/BRICKS.gd
+++ Scripts/BRICKS.gd
@@ -1,14 +1,30 @@
 extends KinematicBody2D
 
-var velocity = Vector2()
+onready var BT=preload("res://Scene/brickTexture.tscn")
+onready var BC=preload("res://Scene/brickCollision.tscn")
 
+var velocity = Vector2()
 var SPEED = 500
 var ACCELERATION = 0
 
+func group():
+	var n=0
+	for i in range(2):
+		for j in range(3):
+			var bt=BT.instance()
+			var bc=BC.instance()
+			bt.name = "BrickTexture "+str(n)
+			bc.name = "BrickCollision "+str(n)
+			bt.position=Vector2(200+64*(j),100+32*(i))
+			bc.position=Vector2(200+64*(j),100+32*(i))
+			add_child(bt)
+			add_child(bc)
+			n+=1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	
+	group()
+
 func _physics_process(delta):
 	
 	var move_input_x = Input.get_axis("P2_move_left","P2_move_right")
@@ -18,6 +34,3 @@ func _physics_process(delta):
 	velocity.y = move_input_y * SPEED
 	
 	velocity=move_and_slide(velocity, Vector2.ZERO).normalized()
-
-func _on_body_entered(body: Node):
-	pass
